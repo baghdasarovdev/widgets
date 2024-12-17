4e570082a2fcdf0b072337bdd89f8f7af5b50c4b
baghdasarovdev <baghdasarov.dev@gmail.com>
2024-12-17 18:35:06 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import "./App.css";
+// import { Calendar } from "./components/Calendar.tsx";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">{/* <Calendar /> */}</div>;
 }
 
 export default App;
