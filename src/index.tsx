a1c8068a31b545ce335206c918f79c0b8f1f4bb0
baghdasarovdev <baghdasarov.dev@gmail.com>
2024-12-17 18:47:47 +0400

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,14 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-root.render(<App />);
+const widgets = document.querySelectorAll(".container-widget");
+
+widgets.forEach((widget) => {
+  const widgetElement = widget as HTMLElement;
+  const root = ReactDOM.createRoot(widgetElement);
+  root.render(
+    <React.StrictMode>
+      <App id={widgetElement.dataset.symbol} />
+    </React.StrictMode>
+  );
+});
