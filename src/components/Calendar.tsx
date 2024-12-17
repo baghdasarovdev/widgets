b61aa828db36f3497a7876bd86fbe67bd417be43
baghdasarovdev <baghdasarov.dev@gmail.com>
2024-12-17 19:02:26 +0400

    first commit

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -38,7 +38,7 @@ export const Calendar: FC<DatePickerT> = (props) => {
   );
 };
 
-const el = document.querySelector("#widget-container");
+const el = document.querySelector("#calendar-widget");
 
 if (el) {
   const root = ReactDOM.createRoot(el);
