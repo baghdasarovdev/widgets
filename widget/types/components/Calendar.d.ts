c4481a304eb1ce7aa25cb77c4a87896121aca990
baghdasarovdev <baghdasarov.dev@gmail.com>
2024-12-18 16:18:44 +0400

    first commit

--- widget/types/components/Calendar.d.ts
+++
@@ -1,7 +0,0 @@
-import { type FC } from "react";
-import "react-datepicker/dist/react-datepicker.css";
-import "./Calendar.css";
-export type DatePickerT = {
-    months: number;
-};
-export declare const Calendar: FC<DatePickerT>;
