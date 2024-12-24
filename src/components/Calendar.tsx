3a63c97b51a9e4c06368a26e595b4645e5901913
baghdasarovdev <baghdasarov.dev@gmail.com>
2024-12-24 17:54:07 +0400

    minify file

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -56,13 +56,6 @@ const Calendar = () => {
         inline
         icon={null}
       />
-      <link
-        rel="stylesheet"
-        href="https://cdnjs.cloudflare.com/ajax/libs/react-datepicker/7.5.0/react-datepicker.min.css"
-        integrity="sha512-mRJDr36tdkaC/Mw4vQlLHb89s0iwGy0vhmZkDhWF6WjkOoD3AAjk1JHr3uR3elMaxxSpNSeRb+0R3/Jdrp0fdA=="
-        crossorigin="anonymous"
-        referrerpolicy="no-referrer"
-      />
     </div>
   );
 };
@@ -176,3 +169,15 @@ const style = document.createElement("style");
 style.type = "text/css";
 style.appendChild(document.createTextNode(css));
 document.head.appendChild(style);
+
+window.onload = function () {
+  var link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href =
+    "https://cdnjs.cloudflare.com/ajax/libs/react-datepicker/7.5.0/react-datepicker.min.css";
+  link.integrity =
+    "sha512-mRJDr36tdkaC/Mw4vQlLHb89s0iwGy0vhmZkDhWF6WjkOoD3AAjk1JHr3uR3elMaxxSpNSeRb+0R3/Jdrp0fdA==";
+  link.crossOrigin = "anonymous";
+  link.referrerPolicy = "no-referrer";
+  document.head.appendChild(link);
+};
