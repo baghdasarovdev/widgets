147c034a13e3119140e1bb226dd2bc010a671dd4
baghdasarovdev <baghdasarov.dev@gmail.com>
2024-12-24 12:33:45 +0400

    add calendar funtional

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -1,42 +1,68 @@
-import React, { type FC, useState } from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
-
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./Calendar.css";
+import { durationDays, monthsOptions } from "./constant.ts";
 
-export type DatePickerT = {
-  months: number;
-};
+const Calendar = () => {
+  const [startDate, setStartDate] = useState<Date | null>(null);
+  const [selectedDates, setSelectedDates] = useState<Date[]>([]);
+  const [calendarCount, setCalendarCount] = useState<number>(1);
 
-const freeMonths = [1, 3, 5, 9, 10, 11, 2, 4, 6, 7, 8, 12];
+  const handleSelectDates = (dates) => {
+    setStartDate(null);
 
-export const Calendar: FC<DatePickerT> = (props) => {
-  const [startDate, setStartDate] = useState<Date | null>(new Date());
+    if (!startDate) {
+      setStartDate(dates);
+    } else {
+      const date1 = new Date(startDate);
+      const date2 = new Date(dates);
+      const duration = durationDays(date1, date2);
 
-  const getDayClass = (date: Date) => {
-    const month = date.getMonth() + 1;
+      let className = `react-datepicker__day--highlighted-custom`;
 
-    if (freeMonths?.includes(month)) {
-      return "free-date";
+      const checkDate = new Date(startDate);
+      const endDate = new Date(dates);
+
+      const updatedRanges = duration.map((date, i) => {
+        let rangeObject = { [className]: [date] };
+
+        if (date.getTime() === checkDate.getTime()) {
+          const newClassName = `${className} start`;
+          rangeObject = { [newClassName]: [date] };
+        } else if (endDate.getTime() === date.getTime()) {
+          const newClassName = `${className} end`;
+          rangeObject = { [newClassName]: [date] };
+        }
+        return rangeObject;
+      });
+
+      setSelectedDates((prev) => [...prev, ...updatedRanges]);
     }
-
-    return "";
   };
 
   return (
     <div className="custom_calendar">
+      <select onChange={(e) => setCalendarCount(Number(e.target.value))}>
+        {monthsOptions.map(({ title, value }) => (
+          <option key={value} value={value}>
+            {title}
+          </option>
+        ))}
+      </select>
       <DatePicker
-        selected={startDate}
-        onChange={(date: Date | null) => setStartDate(date)}
-        dayClassName={(date: Date) => getDayClass(date)}
-        monthsShown={props.months}
+        highlightDates={selectedDates}
+        onChange={(date) => handleSelectDates(date)}
+        monthsShown={calendarCount}
         inline
         icon={null}
       />
     </div>
   );
 };
+
+export default Calendar;
 
 const el = document.querySelectorAll(".container-widget");
 
