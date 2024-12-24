e89f8733c4308edd5a7301f06d54e2aeac39ef2f
baghdasarovdev <baghdasarov.dev@gmail.com>
2024-12-24 16:55:20 +0400

    minify file

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -176,17 +176,3 @@ const style = document.createElement("style");
 style.type = "text/css";
 style.appendChild(document.createTextNode(css));
 document.head.appendChild(style);
-
-var cssId =
-  "https://cdnjs.cloudflare.com/ajax/libs/react-datepicker/7.5.0/react-datepicker.min.css"; // you could encode the css path itself to generate id..
-if (!document.getElementById(cssId)) {
-  var head = document.getElementsByTagName("head")[0];
-  var link = document.createElement("link");
-  link.id = cssId;
-  link.rel = "stylesheet";
-  link.type = "text/css";
-  link.href =
-    "https://cdnjs.cloudflare.com/ajax/libs/react-datepicker/7.5.0/react-datepicker.min.css";
-  link.media = "all";
-  head.appendChild(link);
-}
