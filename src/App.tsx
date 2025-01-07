74c2ef11caae0e015c15db46a40920f8c5340c81
baghdasarovdev <baghdasarov.dev@gmail.com>
2025-01-07 11:39:53 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React from "react";
 import "./App.css";
 import FullCalendar from "./components/calendar/FullCalendar.tsx";
-import Calendar from "./components/calendar/Calendar.tsx";
-import DatePickerModal from "./components/datepicker/DatePickerModal.tsx";
 
 function App() {
   return (
