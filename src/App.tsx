fc7e92e0cdb7b7b55282caf3e277feb0570cf077
baghdasarovdev <baghdasarov.dev@gmail.com>
2025-01-07 12:14:46 +0400

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import FullCalendar from "./components/calendar/FullCalendar.tsx";
 function App() {
   return (
     <div className="App">
-      <FullCalendar />
+      <FullCalendar /> 
     </div>
   );
 }
